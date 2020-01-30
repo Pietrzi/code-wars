965d01c67ba0d4e576293d30ce7cd0145e102737
Pietrzi <pietrzi@op.pl>
2020-01-30 11:54:51 +0100

    another

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
-function search(budget, prices) {
-  return prices.filter(a => a <= budget).toString();
-  // return array of prices that are within budget
-  
-  }
-console.log(search())
+function root(x, n) {
+  return Math.pow(x, 1.0 / n);
+}
+
+
+console.log(root(8, 3))
+
+// function powerOfTwo(x) {
+//   return Math.log2(x) % 1 === 0;
+// }
+
+// function powerOfTwo(x) {
+//   return (Math.log(x)/Math.log(2)) % 1 === 0;
+// }
