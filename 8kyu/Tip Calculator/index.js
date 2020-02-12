c258036a0ec3220ef07f8dfb4863d11bd4614f3d
Pietrzi <pietrzi@op.pl>
2020-02-12 17:28:46 +0100

    sorry mistake, now it is correct

--- 8kyu/Tip Calculator/index.js
+++ 8kyu/Tip Calculator/index.js
@@ -10,3 +10,55 @@
 // The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
 
 // Because you're a nice person, you always round up the tip, regardless of the service.
+
+// solutions
+
+
+// 1
+
+function calculateTip(amount, rating) {
+    let tip = 0;
+    switch (rating.toLowerCase()) {
+    case 'terrible':
+    tip =  0;
+    break;
+    case 'poor':
+    tip = Math.ceil(amount * 0.05);
+    break;
+    case 'good':
+    tip = Math.ceil(amount * 0.1);
+    break;
+    case 'great':
+    tip = Math.ceil(amount * 0.15);
+    break;
+    case 'excellent':
+    tip = Math.ceil(amount * 0.20);
+    break;
+    default:
+    tip = 'Rating not recognised';
+    }
+    return tip;
+}
+
+
+// 2
+
+function calculateTip(amount, rating) {
+    const tips = { 
+      'terrible'  : 0, 
+      'poor'      : .05, 
+      'good'      : .1, 
+      'great'     : .15, 
+      'excellent' : .2 
+    }
+    rating = rating.toLowerCase();
+    return rating in tips ? Math.ceil(amount * tips[rating]) : "Rating not recognised";
+}
+
+
+// 3
+
+let calculateTip = (a,r) => {
+    const i = ['terrible', 'poor', 'good', 'great', 'excellent'].indexOf(r.toLowerCase());
+    return i === -1 ? "Rating not recognised" : Math.ceil(i*a*.05);
+}
