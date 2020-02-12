40251cc147bbd8f7f9788529a034f6924849766d
Pietrzi <pietrzi@op.pl>
2020-02-12 15:08:11 +0100

    fast one

---
+++ 8kyu/Count the Monkeys!/index.js
@@ -0,0 +1,31 @@
+// You take your son to the forest to see the monkeys. You know that there are a certain number there (n), 
+// but your son is too young to just appreciate the full number, he has to start counting them from 1.
+// As a good parent, you will sit and count with him. Given the number (n), 
+// populate an array with all numbers up to and including that number, but excluding zero.
+
+// example monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+// 1
+
+function monkeyCount(n) {
+    let arr = [];
+    for(let i = 1; i <= n; i++) {
+      arr.push(i)
+    }
+    return arr;
+}
+
+
+// 2
+
+function monkeyCount(n) {
+    return Array.from(Array(n), (_,i)=>++i)
+}
+
+
+// 3
+
+function monkeyCount(n) {
+    return [...Array(n).keys()].map(a => a + 1)
+    }
