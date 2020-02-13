f157a02e97184ac769c721dc0f460844c1991aee
Pietrzi <pietrzi@op.pl>
2020-02-13 11:10:15 +0100

    sorry mistake

---
+++ 8kyu/The 'if' function/index.js
@@ -0,0 +1,35 @@
+// You know what keyword I use too much? if! We should make a function called _if, 
+// with its arguments as a logical test and two functions/lambdas where the first function 
+// is executed if the boolean is true, and the second if it's false, like an if/else statement, 
+// so that we don't have to mess around with those nasty keywords! Even so, 
+// It should support truthy/falsy types just like the keyword.
+
+// Example:
+// _if(true, function(){console.log("True")}, function(){console.log("false")})
+// Logs 'True' to the console.
+
+// solutions
+
+
+// 1
+
+function _if(bool, func1, func2) {
+    return bool ? func1() : func2();
+}
+
+
+// 2
+
+const _if = (bool, func1, func2) => bool ? func1() : func2();
+
+
+// 3
+
+function _if(bool, func1, func2) {
+    if (bool) { func1(); }
+    else { func2(); }
+}
+
+
+// 4
+
